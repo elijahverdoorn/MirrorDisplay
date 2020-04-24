83139409fc8f242fa2c026ce4394e32022f05a6d
Elijah Verdoorn <elijah.verdoorn@wework.com>
2020-04-24 16:02:41 -0700

    Remove old weather API key (it's been deactivated)

--- app/src/main/java/com/elijahverdoorn/mirrordisplay/data/source/RemoteWeatherService.kt
+++ app/src/main/java/com/elijahverdoorn/mirrordisplay/data/source/RemoteWeatherService.kt
@@ -18,9 +18,5 @@ interface RemoteWeatherService {
         fun create() = RetrofitService.create(RemoteWeatherService::class.java, WEATHER_BASE_URL)
 
         private const val WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/"
-        private const val WEATHER_API_KEY = "028d7ee980bd54533953095ee6ddbf61"
-
-        private const val OAKLAND_LAT = "37.8044"
-        private const val OAKLAND_LON = "122.2712"
     }
 }
